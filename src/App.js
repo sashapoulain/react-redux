59a1b1c0eff44721177a581b18db6207a0763d10
Sasha Poulain <sashapoulain5@gmail.com>
2024-06-08 20:47:48 +0300

    counter with react-redux

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Counter from './components/Counter';
+import DecreaseCounter from './components/DecreaseCounter';
+import IncreaseCounter from './components/IncreaseCounter';
+import IncreaseByTwoCounter from './components/IncreaseByTwoCounter';
+function App() {
+  return (
+    <div>
+    <Counter/>
+    <IncreaseCounter/>
+    <DecreaseCounter/>
+    <IncreaseByTwoCounter/>
+    
+    </div>
+  );
+}
+
+export default App;
